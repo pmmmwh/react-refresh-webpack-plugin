e1cfe981319824124caa890ebe1684ca02c66ac1
Michael Mok <pmmmwh@gmail.com>
2019-09-09 04:13:36 +0800

    refactor: align naming of react-refresh imports

--- src/utils/ReactRefreshEntry.ts
+++ src/utils/ReactRefreshEntry.ts
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV !== 'production' && typeof window !== 'undefined') {
-  const runtime = require('react-refresh/runtime');
-  runtime.injectIntoGlobalHook(window);
+  const Refresh = require('react-refresh/runtime');
+  Refresh.injectIntoGlobalHook(window);
   (window as any).$RefreshReg$ = (): void => undefined;
   (window as any).$RefreshSig$ = () => <T>(type: T) => type;
 }
