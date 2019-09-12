40a993be6af1fe1c7eb1d6b9a2eca2461f12e6d8
Michael Mok <pmmmwh@gmail.com>
2019-09-13 06:17:54 +0800

    refactor: add module hot detection in loader

--- src/loader/RefreshHotLoader.ts
+++ src/loader/RefreshHotLoader.ts
@@ -2,6 +2,7 @@ const RefreshInjection = `
 const RefreshUtils = require('${require.resolve('./utils')}');
 
 if (
+  module.hot &&
   RefreshUtils.isReactRefreshBoundary(
     module.exports
     || module.__proto__.exports
