e6f9211b399df45dd6988e231b04e8f3e59775e4
Michael Mok <pmmmwh@gmail.com>
2019-09-13 05:06:55 +0800

    feat(loader): add refresh hot loader to inject hot accept code

---
+++ src/loader/RefreshHotLoader.ts
@@ -0,0 +1,19 @@
+const RefreshInjection = `
+const RefreshUtils = require('${require.resolve('./utils')}');
+
+if (
+  RefreshUtils.isReactRefreshBoundary(
+    module.exports
+    || module.__proto__.exports
+  )
+) {
+  module.hot.accept();
+  RefreshUtils.enqueueUpdate()
+}
+`;
+
+function RefreshHotLoader(source: string) {
+  return source + RefreshInjection;
+}
+
+export default RefreshHotLoader;
