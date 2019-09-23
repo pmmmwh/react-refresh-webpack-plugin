ed6ab9f08ae5db0bb7e1fa818113e5c46afbe69d
Michael Mok <pmmmwh@gmail.com>
2019-09-24 04:40:13 +0800

    refactor: update refresh hot loader to take in hot runtime file

--- src/loader/RefreshHotLoader.js
+++ src/loader/RefreshHotLoader.js
@@ -1,28 +1,19 @@
-/**
- * Code injected to each JS-like module for react-refresh capabilities.
- *
- * [Reference for HMR Error Recovery](https://github.com/webpack/webpack/issues/418#issuecomment-490296365)
- */
-// TODO: Is it possible to move this into a file?
-const RefreshInjection = `
-const $RefreshUtils$ = require('${require.resolve('./utils')}');
-
-$RefreshUtils$.registerExportsForReactRefresh(module);
-
-if (module.hot && $RefreshUtils$.isReactRefreshBoundary(module)) {
-  module.hot.accept($RefreshUtils$.createHotErrorHandler(module.id));
-  $RefreshUtils$.performFullRefreshIfNeeded();
-  $RefreshUtils$.enqueueUpdate();
-}
-`;
+const { Template } = require('webpack');
+const { runtimeUtils } = require('../runtime/globals');
 
 /**
- * A simple Webpack loader to inject react-refresh code into modules.
+ * A simple Webpack loader to inject react-refresh HMR code into modules.
  * @param {string} source The original module source code.
  * @returns {string} The injected module source code.
  */
 function RefreshHotLoader(source) {
-  return source + RefreshInjection;
+  return (
+    source +
+    Template.getFunctionContent(require('../runtime/RefreshModuleRuntime')).replace(
+      /\$RefreshUtils\$/g,
+      runtimeUtils
+    )
+  );
 }
 
 module.exports = RefreshHotLoader;
