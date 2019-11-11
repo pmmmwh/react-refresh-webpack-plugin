0accda866b8364971cbd16e03c46e03343b6b563
Michael Mok <pmmmwh@gmail.com>
2019-11-11 08:11:37 +0800

    feat(overlay): implement container for runtime errors

---
+++ src/overlay/containers/RuntimeErrorContainer.js
@@ -0,0 +1,56 @@
+const CloseButton = require('../components/CloseButton');
+const ErrorFooter = require('../components/ErrorFooter');
+const PageHeader = require('../components/PageHeader');
+const ErrorStack = require('../components/RuntimeErrorStack');
+const theme = require('../theme');
+const removeAllChildren = require('../utils/removeAllChildren');
+
+/**
+ * @typedef {Object} RuntimeErrorContainerProps
+ * @property {number} activeErrorIndex
+ * @property {Error[]} errors
+ * @property {function(MouseEvent): *} onClickCloseButton
+ * @property {function(MouseEvent): *} onClickNextButton
+ * @property {function(MouseEvent): *} onClickPrevButton
+ */
+
+/**
+ *
+ * @param {Document} document
+ * @param {HTMLElement} root
+ * @param {RuntimeErrorContainerProps} props
+ * @returns {void}
+ */
+function RuntimeErrorContainer(document, root, props) {
+  removeAllChildren(root);
+
+  const currentError = props.errors[props.activeErrorIndex];
+
+  let errorTitle = 'Caught ' + props.errors.length;
+  if (props.errors.length === 1) {
+    errorTitle += ' error.';
+  } else {
+    errorTitle += ' errors.';
+  }
+
+  CloseButton(document, root, {
+    onClick: props.onClickCloseButton,
+  });
+  ErrorFooter(document, root, {
+    activeErrorIndex: props.activeErrorIndex,
+    errors: props.errors,
+    onNext: props.onClickNextButton,
+    onPrev: props.onClickPrevButton,
+    theme: theme,
+  });
+  PageHeader(document, root, {
+    title: errorTitle,
+    theme: theme,
+  });
+  ErrorStack(document, root, {
+    error: currentError,
+    theme: theme,
+  });
+}
+
+module.exports = RuntimeErrorContainer;
