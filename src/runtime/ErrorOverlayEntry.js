297ecc700e3a54d1b0502845cce90bcb7021d07f
Michael Mok <pmmmwh@gmail.com>
2019-11-11 08:30:11 +0800

    feat: rework integration with error overlay within plugin

--- src/runtime/ErrorOverlayEntry.js
+++ src/runtime/ErrorOverlayEntry.js
@@ -2,72 +2,125 @@
 /* global __resourceQuery */
 
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
-const ErrorOverlay = require('react-error-overlay');
 const stripAnsi = require('strip-ansi');
-const { runtimeGlobalHook } = require('./globals');
+const ErrorOverlay = require('../overlay');
 const createSocket = require('./createSocket');
+const {
+  error: registerErrorHandler,
+  unhandledRejection: registerUnhandledRejectionHandler,
+} = require('./errorEventHandlers');
+const { runtimeGlobalHook } = require('./globals');
 
-// Error Overlay Integration for Compile Errors
-var isHotReload = false;
-var hasCompileErrors = false;
+// Setup Global Hook
+window[runtimeGlobalHook] = window[runtimeGlobalHook] || {};
 
-function handleSuccess() {
+// Setup error states
+let isHotReload = false;
+let hasCompileErrors = false;
+let runtimeErrors = [];
+
+/**
+ * Detects if an error is a Webpack compilation error.
+ * @param {Error} error The error of interest.
+ * @returns {boolean} If the error is a Webpack compilation error.
+ */
+function isWebpackCompileError(error) {
+  return /Module [A-z ]+\(from/.test(error.message);
+}
+
+/**
+ * Try dismissing the compile error overlay.
+ * This will also reset runtime error records because we have new source to evaluate.
+ * @returns {void}
+ */
+function tryDismissCompileErrorOverlay() {
+  if (runtimeErrors.length) {
+    runtimeErrors = [];
+  }
+  if (!hasCompileErrors) {
+    ErrorOverlay.clearCompileError();
+  }
+}
+
+/**
+ * A function called after a compile success signal is received from Webpack.
+ * @returns {void}
+ */
+function handleCompileSuccess() {
   isHotReload = true;
   hasCompileErrors = false;
 
   if (isHotReload) {
-    tryDismissErrorOverlay();
+    tryDismissCompileErrorOverlay();
   }
 }
 
-function handleErrors(errors) {
+/**
+ * A function called after a compile errored signal is received from Webpack.
+ * @param {string} errors
+ * @returns {void}
+ */
+function handleCompileErrors(errors) {
   isHotReload = true;
   hasCompileErrors = true;
 
-  var formatted = formatWebpackMessages({
+  const formatted = formatWebpackMessages({
     errors: errors,
     warnings: [],
   });
 
   // Only show the first error
-  ErrorOverlay.reportBuildError(formatted.errors[0]);
+  ErrorOverlay.showCompileError(formatted.errors[0]);
 
-  if (typeof console !== 'undefined' && typeof console.error === 'function') {
-    for (var i = 0; i < formatted.errors.length; i += 1) {
-      console.error(stripAnsi(formatted.errors[i]));
-    }
+  for (let i = 0; i < formatted.errors.length; i += 1) {
+    console.error(stripAnsi(formatted.errors[i]));
   }
 
   // Do not attempt to reload now.
   // We will reload on next success instead.
 }
 
-function tryDismissErrorOverlay() {
-  if (!hasCompileErrors) {
-    ErrorOverlay.dismissBuildError();
-  }
-}
-
-createSocket(function messageHandler(message) {
+/**
+ * Handles compilation messages from Webpack.
+ * Integrates with a compile error overlay.
+ * @param {*} message A Webpack HMR message sent via WebSockets.
+ * @returns {void}
+ */
+function compileMessageHandler(message) {
   switch (message.type) {
     case 'ok':
-      handleSuccess();
+      handleCompileSuccess();
       break;
     case 'errors':
-      handleErrors(message.data);
+      handleCompileErrors(message.data);
       break;
     default:
     // Do nothing.
   }
-});
+}
 
-// Error Overlay Integration for Runtime Errors
-// Setup Global Hook
-window[runtimeGlobalHook] = window[runtimeGlobalHook] || {};
-window[runtimeGlobalHook].hasRuntimeErrors = false;
-
-ErrorOverlay.startReportingRuntimeErrors({
-  onError: function() {
+/**
+ * Handles runtime error contexts captured with EventListeners.
+ * Integrates with a runtime error overlay.
+ * @param {error} context A valid error object.
+ * @returns {void}
+ */
+function runtimeErrorHandler(context) {
+  if (!window[runtimeGlobalHook].hasRuntimeErrors) {
     window[runtimeGlobalHook].hasRuntimeErrors = true;
-  },
-});
+  }
+  if (
+    context &&
+    !isWebpackCompileError(context) &&
+    runtimeErrors.indexOf(context) === -1
+  ) {
+    runtimeErrors = runtimeErrors.concat(context);
+  }
+  ErrorOverlay.showRuntimeErrors(runtimeErrors);
+}
+
+// Registers handlers for compile errors
+createSocket(compileMessageHandler);
+// Registers handlers for runtime errors
+registerErrorHandler(runtimeErrorHandler);
+registerUnhandledRejectionHandler(runtimeErrorHandler);
