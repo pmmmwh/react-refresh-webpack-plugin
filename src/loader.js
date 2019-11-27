303c42c8fa5947cb668c5879aa5a922d25b3533a
Michael Mok <pmmmwh@gmail.com>
2019-11-28 05:00:35 +0800

    fix: make sourcemaps work with the plugin

--- src/loader.js
+++ src/loader.js
@@ -7,21 +7,32 @@ const reactModule = /['"]react['"]/;
 
 /**
  * A simple Webpack loader to inject react-refresh HMR code into modules.
+ *
+ * [Reference for Loader API](https://webpack.js.org/api/loaders/)
  * @param {string} source The original module source code.
+ * @param {*} [inputSourceMap] The source map of the module.
+ * @property {function(string): void} addDependency Adds a dependency for Webpack to watch.
+ * @property {function(Error | null, string | Buffer, *?, *?): void} callback Sends loader results to Webpack.
  * @returns {string} The injected module source code.
  */
-function RefreshHotLoader(source) {
+function RefreshHotLoader(source, inputSourceMap) {
   // Add dependency to allow caching and invalidations
   this.addDependency(path.resolve('./runtime/RefreshModuleRuntime'));
 
-  // Only apply transform if the source code contains a React import
-  return reactModule.test(source)
-    ? source +
-        '\n' +
-        Template.getFunctionContent(require('./runtime/RefreshModuleRuntime'))
-          .trim()
-          .replace(/\$RefreshUtils\$/g, refreshUtils)
-    : source;
+  // Use callback to allow source maps to pass through
+  this.callback(
+    null,
+    // Only apply transform if the source code contains a React import
+    reactModule.test(source)
+      ? source +
+          '\n\n' +
+          Template.getFunctionContent(require('./runtime/RefreshModuleRuntime'))
+            .trim()
+            .replace(/^ {2}/gm, '')
+            .replace(/\$RefreshUtils\$/g, refreshUtils)
+      : source,
+    inputSourceMap
+  );
 }
 
 module.exports = RefreshHotLoader;
