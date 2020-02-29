c9c046fb461dab048da5b13415c71bdb1ab15a87
Adam Charron <adam@charrondev.com>
2020-02-28 20:28:34 -0500

    Add CRA integration

--- examples/cra-kitchen-sink/config-overrides.js
+++ examples/cra-kitchen-sink/config-overrides.js
@@ -1,7 +1,23 @@
-const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const { addBabelPlugin, addWebpackPlugin, override } = require('customize-cra');
+
+// When using from NPM you should import this as follows:
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const replaceCRADevServer = () => config => {
+  config.entry = config.entry.map(entry => {
+    if (entry.match(/react-dev-utils/)) {
+
+      // When using from NPM you should resolve this as follows:
+      // return require.resolve("@pmmmwh/react-refresh-webpack-plugin/src/webpackHotDevClient");
+      return require.resolve("../../src/webpackHotDevClient");
+    }
+    return entry;
+  })
+  return config;
+}
 
 module.exports = override(
   addBabelPlugin(require.resolve('react-refresh/babel')),
-  addWebpackPlugin(new ReactRefreshPlugin())
+  addWebpackPlugin(new ReactRefreshPlugin({ disableRefreshCheck: true })),
+  replaceCRADevServer(),
 );
