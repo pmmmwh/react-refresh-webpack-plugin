daa225e31cdce9f2df881ca18a201a46a7b6f927
Michael Mok <pmmmwh@gmail.com>
2020-04-02 06:50:34 +0800

    fix: make types.js a module and add missing socket types

--- src/types.js
+++ src/types.js
@@ -9,5 +9,10 @@
  * @property {boolean} [disableRefreshCheck] Disables detection of react-refresh's Babel plugin. (Deprecated since v0.3.0)
  * @property {boolean} [forceEnable] Enables the plugin forcefully.
  * @property {boolean | ErrorOverlayOptions} [overlay] Modifies how the error overlay integration works in the plugin.
+ * @property {string} [sockHost] The socket host to use for the error overlay integration.
+ * @property {string} [sockPath] The socket path to use for the error overlay integration.
+ * @property {number} [sockPort] The socket port to use for the error overlay integration.
  * @property {boolean} [useLegacyWDSSockets] Uses a custom SocketJS implementation for older versions of webpack-dev-server.
  */
+
+module.exports = {};
