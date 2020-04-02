797dbdb6c690a07305c8e952b44733753519792b
Michael Mok <pmmmwh@gmail.com>
2020-04-03 00:30:48 +0800

    feat!: update types to make socket options scoped under overlay

--- src/types.js
+++ src/types.js
@@ -1,7 +1,10 @@
 /**
  * @typedef {Object} ErrorOverlayOptions
  * @property {string} [entry] Path to a JS file that sets up the error overlay integration.
- * @property {string} module The error overlay module to use.
+ * @property {string} [module] The error overlay module to use.
+ * @property {string} [sockHost] The socket host to use.
+ * @property {string} [sockPath] The socket path to use.
+ * @property {number} [sockPort] The socket port to use.
  */
 
 /**
@@ -9,9 +12,6 @@
  * @property {boolean} [disableRefreshCheck] Disables detection of react-refresh's Babel plugin. (Deprecated since v0.3.0)
  * @property {boolean} [forceEnable] Enables the plugin forcefully.
  * @property {boolean | ErrorOverlayOptions} [overlay] Modifies how the error overlay integration works in the plugin.
- * @property {string} [sockHost] The socket host to use for the error overlay integration.
- * @property {string} [sockPath] The socket path to use for the error overlay integration.
- * @property {number} [sockPort] The socket port to use for the error overlay integration.
  * @property {boolean} [useLegacyWDSSockets] Uses a custom SocketJS implementation for older versions of webpack-dev-server.
  */
 
