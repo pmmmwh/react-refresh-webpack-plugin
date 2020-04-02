6bc63dfb0546e74d592f1b2fd6490a8f3cb7f32d
Michael Mok <pmmmwh@gmail.com>
2020-04-03 01:47:48 +0800

    chore: update type definitions for loader to fix issue with "this"

--- src/loader/index.js
+++ src/loader/index.js
@@ -1,5 +1,6 @@
 const { Template } = require('webpack');
 const { refreshUtils } = require('../runtime/globals');
+
 const RefreshModuleRuntime = Template.getFunctionContent(require('./RefreshModuleRuntime'))
   .trim()
   .replace(/^ {2}/gm, '')
@@ -9,11 +10,10 @@ const RefreshModuleRuntime = Template.getFunctionContent(require('./RefreshModul
  * A simple Webpack loader to inject react-refresh HMR code into modules.
  *
  * [Reference for Loader API](https://webpack.js.org/api/loaders/)
+ * @this {import('webpack').loader.LoaderContext}
  * @param {string} source The original module source code.
- * @param {*} [inputSourceMap] The source map of the module.
- * @property {function(string): void} addDependency Adds a dependency for Webpack to watch.
- * @property {function(Error | null, string | Buffer, *?, *?): void} callback Sends loader results to Webpack.
- * @returns {string} The injected module source code.
+ * @param {import('source-map').RawSourceMap} [inputSourceMap] The source map of the module.
+ * @returns {void}
  */
 function RefreshHotLoader(source, inputSourceMap) {
   // Use callback to allow source maps to pass through
