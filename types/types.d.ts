c87efce716c460de5f2d9f2f7c39103563500d2f
Michael Mok <pmmmwh@gmail.com>
2020-04-18 05:10:42 +0800

    chore: format type files

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,86 +1,88 @@
 export type ErrorOverlayOptions = {
-    /**
-     * Path to a JS file that sets up the error overlay integration.
-     */
-    entry?: string;
-    /**
-     * The error overlay module to use.
-     */
-    module?: string;
-    /**
-     * The socket host to use (WDS only).
-     */
-    sockHost?: string;
-    /**
-     * Path to a JS file that sets up the Webpack socket integration.
-     */
-    sockIntegration?: import("type-fest").LiteralUnion<"wds" | "whm", string>;
-    /**
-     * The socket path to use (WDS only).
-     */
-    sockPath?: string;
-    /**
-     * The socket port to use (WDS only).
-     */
-    sockPort?: number;
+  /**
+   * Path to a JS file that sets up the error overlay integration.
+   */
+  entry?: string;
+  /**
+   * The error overlay module to use.
+   */
+  module?: string;
+  /**
+   * The socket host to use (WDS only).
+   */
+  sockHost?: string;
+  /**
+   * Path to a JS file that sets up the Webpack socket integration.
+   */
+  sockIntegration?: import('type-fest').LiteralUnion<'wds' | 'whm', string>;
+  /**
+   * The socket path to use (WDS only).
+   */
+  sockPath?: string;
+  /**
+   * The socket port to use (WDS only).
+   */
+  sockPort?: number;
 };
 export type NormalizedErrorOverlayOptions = {
-    /**
-     * The socket host to use (WDS only).
-     */
-    sockHost?: string | undefined;
-    /**
-     * The socket path to use (WDS only).
-     */
-    sockPath?: string | undefined;
-    /**
-     * The socket port to use (WDS only).
-     */
-    sockPort?: number | undefined;
-    /**
-     * The error overlay module to use.
-     */
-    module: string;
-    /**
-     * Path to a JS file that sets up the error overlay integration.
-     */
-    entry: string;
-    /**
-     * Path to a JS file that sets up the Webpack socket integration.
-     */
-    sockIntegration: import("type-fest").LiteralUnion<"wds" | "whm", string>;
+  /**
+   * The socket host to use (WDS only).
+   */
+  sockHost?: string | undefined;
+  /**
+   * The socket path to use (WDS only).
+   */
+  sockPath?: string | undefined;
+  /**
+   * The socket port to use (WDS only).
+   */
+  sockPort?: number | undefined;
+  /**
+   * The error overlay module to use.
+   */
+  module: string;
+  /**
+   * Path to a JS file that sets up the error overlay integration.
+   */
+  entry: string;
+  /**
+   * Path to a JS file that sets up the Webpack socket integration.
+   */
+  sockIntegration: import('type-fest').LiteralUnion<'wds' | 'whm', string>;
 };
 export type ReactRefreshPluginOptions = {
-    /**
-     * Disables detection of react-refresh's Babel plugin (Deprecated since v0.3.0).
-     */
-    disableRefreshCheck?: boolean;
-    /**
-     * Files to explicitly exclude from processing.
-     */
-    exclude?: string | RegExp | (string | RegExp)[];
-    /**
-     * Enables the plugin forcefully.
-     */
-    forceEnable?: boolean;
-    /**
-     * Files to explicitly include for processing.
-     */
-    include?: string | RegExp | (string | RegExp)[];
-    /**
-     * Modifies how the error overlay integration works in the plugin.
-     */
-    overlay?: boolean | ErrorOverlayOptions;
-    /**
-     * Uses a custom SocketJS implementation for older versions of webpack-dev-server.
-     */
-    useLegacyWDSSockets?: boolean;
+  /**
+   * Disables detection of react-refresh's Babel plugin (Deprecated since v0.3.0).
+   */
+  disableRefreshCheck?: boolean;
+  /**
+   * Files to explicitly exclude from processing.
+   */
+  exclude?: string | RegExp | (string | RegExp)[];
+  /**
+   * Enables the plugin forcefully.
+   */
+  forceEnable?: boolean;
+  /**
+   * Files to explicitly include for processing.
+   */
+  include?: string | RegExp | (string | RegExp)[];
+  /**
+   * Modifies how the error overlay integration works in the plugin.
+   */
+  overlay?: boolean | ErrorOverlayOptions;
+  /**
+   * Uses a custom SocketJS implementation for older versions of webpack-dev-server.
+   */
+  useLegacyWDSSockets?: boolean;
 };
 export type OverlayOverrides = {
-    /**
-     * Modifies how the error overlay integration works in the plugin.
-     */
-    overlay: false | {
+  /**
+   * Modifies how the error overlay integration works in the plugin.
+   */
+  overlay:
+    | false
+    | {
         /**
          * The socket host to use (WDS only).
          */
@@ -104,10 +106,11 @@ export type OverlayOverrides = {
         /**
          * Path to a JS file that sets up the Webpack socket integration.
          */
-        sockIntegration: import("type-fest").LiteralUnion<"wds" | "whm", string>;
-    };
+        sockIntegration: import('type-fest').LiteralUnion<'wds' | 'whm', string>;
+      };
 };
-export type NormalizedPluginOptions = Pick<{
+export type NormalizedPluginOptions = Pick<
+  {
     /**
      * Disables detection of react-refresh's Babel plugin (Deprecated since v0.3.0).
      */
@@ -132,4 +135,7 @@ export type NormalizedPluginOptions = Pick<{
      * Files to explicitly exclude from processing.
      */
     exclude: string | RegExp | (string | RegExp)[];
-}, "include" | "exclude" | "disableRefreshCheck" | "forceEnable" | "useLegacyWDSSockets"> & OverlayOverrides;
+  },
+  'include' | 'exclude' | 'disableRefreshCheck' | 'forceEnable' | 'useLegacyWDSSockets'
+> &
+  OverlayOverrides;
