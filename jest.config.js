d9500143f1d880db063276a4486bcb7f91182658
Michael Mok <pmmmwh@gmail.com>
2020-05-15 08:32:07 +0800

    test: add jest config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  modulePaths: [],
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/test/**/*.test.js'],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+};
