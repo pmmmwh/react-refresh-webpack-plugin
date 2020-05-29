6c08bba2452a9e7a5d09a71923b12ae9817e6880
Michael Mok <pmmmwh@gmail.com>
2020-05-30 01:39:50 +0800

    refactor: remove placeholder symbols in refresh module runtime

--- src/loader/RefreshModule.runtime.js
+++ src/loader/RefreshModule.runtime.js
@@ -1,4 +1,4 @@
-/* global $RefreshUtils$ */
+/* global __react_refresh_utils__ */
 
 /**
  * Code appended to each JS-like module for react-refresh capabilities.
@@ -11,29 +11,29 @@
  * [Reference for HMR Error Recovery](https://github.com/webpack/webpack/issues/418#issuecomment-490296365)
  */
 module.exports = function () {
-  const currentExports = $RefreshUtils$.getModuleExports(module);
-  $RefreshUtils$.registerExportsForReactRefresh(currentExports, module.id);
+  const currentExports = __react_refresh_utils__.getModuleExports(module);
+  __react_refresh_utils__.registerExportsForReactRefresh(currentExports, module.id);
 
   if (module.hot) {
     const isHotUpdate = !!module.hot.data;
     const prevExports = isHotUpdate ? module.hot.data.prevExports : null;
 
-    if ($RefreshUtils$.isReactRefreshBoundary(currentExports)) {
-      module.hot.dispose($RefreshUtils$.createHotDisposeCallback(currentExports));
-      module.hot.accept($RefreshUtils$.createHotErrorHandler(module.id));
+    if (__react_refresh_utils__.isReactRefreshBoundary(currentExports)) {
+      module.hot.dispose(__react_refresh_utils__.createHotDisposeCallback(currentExports));
+      module.hot.accept(__react_refresh_utils__.createHotErrorHandler(module.id));
 
       if (isHotUpdate) {
         if (
-          $RefreshUtils$.isReactRefreshBoundary(prevExports) &&
-          $RefreshUtils$.shouldInvalidateReactRefreshBoundary(prevExports, currentExports)
+          __react_refresh_utils__.isReactRefreshBoundary(prevExports) &&
+          __react_refresh_utils__.shouldInvalidateReactRefreshBoundary(prevExports, currentExports)
         ) {
           module.hot.invalidate();
         } else {
-          $RefreshUtils$.enqueueUpdate();
+          __react_refresh_utils__.enqueueUpdate();
         }
       }
     } else {
-      if (isHotUpdate && $RefreshUtils$.isReactRefreshBoundary(prevExports)) {
+      if (isHotUpdate && __react_refresh_utils__.isReactRefreshBoundary(prevExports)) {
         module.hot.invalidate();
       }
     }
