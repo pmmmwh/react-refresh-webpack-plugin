0811a264846dfd65f4b15d6651c76ca02d3454d8
Michael Mok <pmmmwh@gmail.com>
2020-06-15 19:53:46 +0800

    fix: generate identity source map when inputSourceMap is undefined

--- src/loader/index.js
+++ src/loader/index.js
@@ -1,5 +1,32 @@
-const { SourceMapConsumer, SourceNode } = require('source-map');
+const { SourceMapConsumer, SourceMapGenerator, SourceNode } = require('source-map');
 const { Template } = require('webpack');
+
+/**
+ * Generates an identity source map from a source file.
+ * @param {string} source The content of the source file.
+ * @param {string} resourcePath The name of the source file.
+ * @return {import('source-map').RawSourceMap} The identity source map.
+ */
+function getIdentitySourceMap(source, resourcePath) {
+  const sourceMap = new SourceMapGenerator();
+  sourceMap.setSourceContent(resourcePath, source);
+
+  source.split('\n').forEach((line, index) => {
+    sourceMap.addMapping({
+      source: resourcePath,
+      original: {
+        line: index + 1,
+        column: 0,
+      },
+      generated: {
+        line: index + 1,
+        column: 0,
+      },
+    });
+  });
+
+  return sourceMap.toJSON();
+}
 
 /**
  * Gets a runtime template from provided function.
@@ -28,9 +55,14 @@ function ReactRefreshLoader(source, inputSourceMap, meta) {
 
   async function _loader(source, inputSourceMap) {
     if (this.sourceMap) {
+      let originalSourceMap = inputSourceMap;
+      if (!originalSourceMap) {
+        originalSourceMap = getIdentitySourceMap(source, this.resourcePath);
+      }
+
       const node = SourceNode.fromStringWithSourceMap(
         source,
-        await new SourceMapConsumer(inputSourceMap)
+        await new SourceMapConsumer(originalSourceMap)
       );
 
       node.prepend([RefreshSetupRuntime, '\n\n']);
