cc5a0f44690f0cace72389706b123936a8698944
Michael Mok <pmmmwh@gmail.com>
2020-06-26 04:32:09 +0800

    test: add describe.skipIf helper

--- test/jest-test-setup.js
+++ test/jest-test-setup.js
@@ -1,10 +1,24 @@
 /**
+ * Skips a test block conditionally.
+ * @param {boolean} condition The condition to skip the test block.
+ * @param {string} blockName The name of the test block.
+ * @param {import('@jest/types').Global.BlockFn} blockFn The test block function.
+ * @return {void}
+ */
+describe.skipIf = (condition, blockName, blockFn) => {
+  if (condition) {
+    return describe.skip(blockName, blockFn);
+  }
+  return describe(blockName, blockFn);
+};
+
+/**
  * Skips a test conditionally.
  * @param {boolean} condition The condition to skip the test.
  * @param {string} testName The name of the test.
  * @param {import('@jest/types').Global.TestFn} fn The test function.
  * @param {number} [timeout] The time to wait before aborting.
- * @return {void|*}
+ * @return {void}
  */
 test.skipIf = (condition, testName, fn, timeout) => {
   if (condition) {
