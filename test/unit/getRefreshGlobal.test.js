aecdf79626a11447fffce4a2e238fd3fe95ed57b
Michael Mok <pmmmwh@gmail.com>
2020-07-13 05:08:28 +0800

    fix: runtime injection issues (#140)

--- test/unit/getRefreshGlobal.test.js
+++ test/unit/getRefreshGlobal.test.js
@@ -9,13 +9,16 @@ describe('getRefreshGlobal', () => {
     delete global.__webpack_require__;
   });
 
-  it('should return refresh global template without providing runtime template', () => {
-    const refreshGlobal = getRefreshGlobal();
-    expect(refreshGlobal).toMatchInlineSnapshot(`
+  it('should return working refresh global without providing runtime template', () => {
+    const refreshGlobalTemplate = getRefreshGlobal();
+    expect(refreshGlobalTemplate).toMatchInlineSnapshot(`
       "__webpack_require__.$Refresh$ = {
-      	cleanup: function() { return undefined; },
-      	register: function() { return undefined; },
-      	runtime: {},
+      	init: function() {
+      		__webpack_require__.$Refresh$.cleanup = function() { return undefined; };
+      		__webpack_require__.$Refresh$.register = function() { return undefined; };
+      		__webpack_require__.$Refresh$.runtime = {};
+      		__webpack_require__.$Refresh$.signature = function() { return function(type) { return type; }; };
+      	},
       	setup: function(currentModuleId) {
       		var prevCleanup = __webpack_require__.$Refresh$.cleanup;
       		var prevReg = __webpack_require__.$Refresh$.register;
@@ -28,57 +31,80 @@ describe('getRefreshGlobal', () => {
 
       		__webpack_require__.$Refresh$.signature = __webpack_require__.$Refresh$.runtime.createSignatureFunctionForTransform;
 
-      		__webpack_require__.$Refresh$.cleanup = function() {
-      			__webpack_require__.$Refresh$.register = prevReg;
-      			__webpack_require__.$Refresh$.signature = prevSig;
-      			__webpack_require__.$Refresh$.cleanup = prevCleanup;
+      		__webpack_require__.$Refresh$.cleanup = function(cleanupModuleId) {
+      			if (currentModuleId === cleanupModuleId) {
+      				__webpack_require__.$Refresh$.register = prevReg;
+      				__webpack_require__.$Refresh$.signature = prevSig;
+      				__webpack_require__.$Refresh$.cleanup = prevCleanup;
+      			}
       		}
       	},
-      	signature: function() { return function(type) { return type; }; }
       };"
     `);
     expect(() => {
-      eval(refreshGlobal);
+      eval(refreshGlobalTemplate);
     }).not.toThrow();
+
+    const refreshGlobal = global.__webpack_require__.$Refresh$;
+    expect(() => {
+      refreshGlobal.init();
+    }).not.toThrow();
+    expect(typeof refreshGlobal.cleanup).toBe('function');
+    expect(typeof refreshGlobal.register).toBe('function');
+    expect(typeof refreshGlobal.signature).toBe('function');
+    expect(typeof refreshGlobal.runtime).toBe('object');
   });
 
   it.skipIf(
     WEBPACK_VERSION !== 5,
-    'should return refresh global template with provided runtime template',
+    'should return working refresh global with provided runtime template',
     () => {
       const RuntimeTemplate = require('webpack/lib/RuntimeTemplate');
-      const refreshGlobal = getRefreshGlobal(
+      const refreshGlobalTemplate = getRefreshGlobal(
         new RuntimeTemplate({ ecmaVersion: 6 }, { shorten: (item) => item })
       );
-      expect(refreshGlobal).toMatchInlineSnapshot(`
-      "__webpack_require__.$Refresh$ = {
-      	cleanup: () => undefined,
-      	register: () => undefined,
-      	runtime: {},
-      	setup: (currentModuleId) => {
-      		const prevCleanup = __webpack_require__.$Refresh$.cleanup;
-      		const prevReg = __webpack_require__.$Refresh$.register;
-      		const prevSig = __webpack_require__.$Refresh$.signature;
+      expect(refreshGlobalTemplate).toMatchInlineSnapshot(`
+        "__webpack_require__.$Refresh$ = {
+        	init: () => {
+        		__webpack_require__.$Refresh$.cleanup = () => undefined;
+        		__webpack_require__.$Refresh$.register = () => undefined;
+        		__webpack_require__.$Refresh$.runtime = {};
+        		__webpack_require__.$Refresh$.signature = () => (type) => type;
+        	},
+        	setup: (currentModuleId) => {
+        		const prevCleanup = __webpack_require__.$Refresh$.cleanup;
+        		const prevReg = __webpack_require__.$Refresh$.register;
+        		const prevSig = __webpack_require__.$Refresh$.signature;
 
-      		__webpack_require__.$Refresh$.register = (type, id) => {
-      			const typeId = currentModuleId + \\" \\" + id;
-      			__webpack_require__.$Refresh$.runtime.register(type, typeId);
-      		}
+        		__webpack_require__.$Refresh$.register = (type, id) => {
+        			const typeId = currentModuleId + \\" \\" + id;
+        			__webpack_require__.$Refresh$.runtime.register(type, typeId);
+        		}
 
-      		__webpack_require__.$Refresh$.signature = __webpack_require__.$Refresh$.runtime.createSignatureFunctionForTransform;
+        		__webpack_require__.$Refresh$.signature = __webpack_require__.$Refresh$.runtime.createSignatureFunctionForTransform;
 
-      		__webpack_require__.$Refresh$.cleanup = () => {
-      			__webpack_require__.$Refresh$.register = prevReg;
-      			__webpack_require__.$Refresh$.signature = prevSig;
-      			__webpack_require__.$Refresh$.cleanup = prevCleanup;
-      		}
-      	},
-      	signature: () => (type) => type
-      };"
-    `);
+        		__webpack_require__.$Refresh$.cleanup = (cleanupModuleId) => {
+        			if (currentModuleId === cleanupModuleId) {
+        				__webpack_require__.$Refresh$.register = prevReg;
+        				__webpack_require__.$Refresh$.signature = prevSig;
+        				__webpack_require__.$Refresh$.cleanup = prevCleanup;
+        			}
+        		}
+        	},
+        };"
+      `);
       expect(() => {
-        eval(refreshGlobal);
+        eval(refreshGlobalTemplate);
       }).not.toThrow();
+
+      const refreshGlobal = global.__webpack_require__.$Refresh$;
+      expect(() => {
+        refreshGlobal.init();
+      }).not.toThrow();
+      expect(typeof refreshGlobal.cleanup).toBe('function');
+      expect(typeof refreshGlobal.register).toBe('function');
+      expect(typeof refreshGlobal.signature).toBe('function');
+      expect(typeof refreshGlobal.runtime).toBe('object');
     }
   );
 });
