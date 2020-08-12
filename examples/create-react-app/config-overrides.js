ea573ba84c9eb14ed1137126d8573417abe7bcd0
Aaron Agarunov <agarunov.aaron@gmail.com>
2020-08-12 14:02:42 -0400

    Remove variable

--- examples/create-react-app/config-overrides.js
+++ examples/create-react-app/config-overrides.js
@@ -2,9 +2,8 @@ const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const { addBabelPlugin, addWebpackPlugin, override } = require('customize-cra');
 
 const isDevelopment = process.env.NODE_ENV === 'development';
-const isEnvDevelopment = webpackEnv === 'development';
 
 module.exports = override(
-  isEnvDevelopment && addBabelPlugin(require.resolve('react-refresh/babel')),
-  isEnvDevelopment && addWebpackPlugin(new ReactRefreshPlugin())
+  isDevelopment && addBabelPlugin(require.resolve('react-refresh/babel')),
+  isDevelopment && addWebpackPlugin(new ReactRefreshPlugin())
 );
