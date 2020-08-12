96e5b4f4ddb47ecbe3da2b4c20500235d95c8c5c
Aaron Agarunov <agarunov.aaron@gmail.com>
2020-08-12 15:07:07 -0400

    Enable react-refresh plugins only in development (#180)
    * Enable react-refresh plugins only in development
    
    * Remove `webpackEnv` variable
    
    Co-authored-by: Michael Mok <pmmmwh@gmail.com>
    
    * Remove variable
    
    Co-authored-by: Michael Mok <pmmmwh@gmail.com>

--- examples/create-react-app/config-overrides.js
+++ examples/create-react-app/config-overrides.js
@@ -1,7 +1,9 @@
 const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const { addBabelPlugin, addWebpackPlugin, override } = require('customize-cra');
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 module.exports = override(
-  addBabelPlugin(require.resolve('react-refresh/babel')),
-  addWebpackPlugin(new ReactRefreshPlugin())
+  isDevelopment && addBabelPlugin(require.resolve('react-refresh/babel')),
+  isDevelopment && addWebpackPlugin(new ReactRefreshPlugin())
 );
