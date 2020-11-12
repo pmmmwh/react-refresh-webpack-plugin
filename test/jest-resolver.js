52516a2fc886e241fd89159312852ee0998c6826
Michael Mok <pmmmwh@gmail.com>
2020-11-12 02:57:06 +0100

    test: add resolver to fix new tests from using require.resolve

---
+++ test/jest-resolver.js
@@ -0,0 +1,12 @@
+function resolver(request, options) {
+  // This acts as a mock for `require.resolve('react-refresh/runtime')`,
+  // since the current mocking behaviour of Jest is not symmetrical,
+  // i.e. only `require` is mocked but not `require.resolve`.
+  if (request === 'react-refresh/runtime') {
+    return 'react-refresh/runtime';
+  }
+
+  return options.defaultResolver(request, options);
+}
+
+module.exports = resolver;
