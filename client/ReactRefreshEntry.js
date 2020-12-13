83f698df0c9892dcb752b56d8c08e2c80d2c7e54
Michael Mok <pmmmwh@gmail.com>
2020-12-13 02:12:13 +0100

    feat: implement monorepo compatibility layer with library

--- client/ReactRefreshEntry.js
+++ client/ReactRefreshEntry.js
@@ -1,13 +1,21 @@
+/* global __react_refresh_library__ */
+
 const safeThis = require('./utils/safeThis');
 
 if (process.env.NODE_ENV !== 'production' && typeof safeThis !== 'undefined') {
+  let $RefreshInjected$ = '__reactRefreshInjected';
+  // Namespace the injected flag (if necessary) for monorepo compatibility
+  if (typeof __react_refresh_library__ !== 'undefined' && __react_refresh_library__) {
+    $RefreshInjected$ += '_' + __react_refresh_library__;
+  }
+
   // Only inject the runtime if it hasn't been injected
-  if (!safeThis.__reactRefreshInjected) {
+  if (!safeThis[$RefreshInjected$]) {
     const RefreshRuntime = require('react-refresh/runtime');
     // Inject refresh runtime into global scope
     RefreshRuntime.injectIntoGlobalHook(safeThis);
 
     // Mark the runtime as injected to prevent double-injection
-    safeThis.__reactRefreshInjected = true;
+    safeThis[$RefreshInjected$] = true;
   }
 }
