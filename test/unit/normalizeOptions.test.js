09783d988a715c90993e059aa06cc04ed0ea2248
Michael Mok <pmmmwh@gmail.com>
2020-12-24 23:25:47 +0100

    refactor!: remove disableRefreshCheck option (#285)

--- test/unit/normalizeOptions.test.js
+++ test/unit/normalizeOptions.test.js
@@ -54,22 +54,6 @@ describe('normalizeOptions', () => {
     });
   });
 
-  // TODO: Remove when the deprecation warning is removed
-  it('should emit warning and exclude its value when disableRefreshCheck is used', () => {
-    jest.spyOn(console, 'warn').mockImplementationOnce(() => undefined);
-
-    expect(normalizeOptions({ disableRefreshCheck: true })).toStrictEqual(DEFAULT_OPTIONS);
-    expect(console.warn).toHaveBeenCalledTimes(1);
-    expect(console.warn).toHaveBeenLastCalledWith(
-      [
-        'The "disableRefreshCheck" option has been deprecated and will not have any effect on how the plugin parses files.',
-        'Please remove it from your configuration.',
-      ].join(' ')
-    );
-
-    console.warn.mockRestore();
-  });
-
   it('should return default for overlay options when it is true', () => {
     expect(normalizeOptions({ overlay: true })).toStrictEqual(DEFAULT_OPTIONS);
   });
