df2d80442bda5b9733be44cb28208481d0679bcf
Michael Mok <pmmmwh@gmail.com>
2021-03-01 10:30:07 +0100

    chore(deps)!: bump the world (#321)

--- types/types.d.ts
+++ types/types.d.ts
@@ -15,13 +15,7 @@ export type ErrorOverlayOptions = {
    * Path to a JS file that sets up the Webpack socket integration.
    */
   sockIntegration?:
-    | false
-    | (string & {
-        _?: undefined;
-      })
-    | 'wds'
-    | 'whm'
-    | 'wps'
+    | import('type-fest').LiteralUnion<false | 'wds' | 'whm' | 'wps', string>
     | undefined;
   /**
    * The socket path to use (WDS only).
@@ -44,44 +38,10 @@ export type ErrorOverlayOptions = {
    */
   useURLPolyfill?: boolean | undefined;
 };
-export type NormalizedErrorOverlayOptions = {
-  /**
-   * The socket host to use (WDS only).
-   */
-  sockHost?: string | undefined;
-  /**
-   * The socket path to use (WDS only).
-   */
-  sockPath?: string | undefined;
-  /**
-   * The socket port to use (WDS only).
-   */
-  sockPort?: number | undefined;
-  /**
-   * The socket protocol to use (WDS only).
-   */
-  sockProtocol?: 'http' | 'https' | 'ws' | 'wss' | undefined;
-  /**
-   * Uses a custom SocketJS implementation for older versions of WDS.
-   */
-  useLegacyWDSSockets?: boolean | undefined;
-  /**
-   * Uses a polyfill for the DOM URL API (WDS only).
-   */
-  useURLPolyfill?: boolean | undefined;
-  /**
-   * The error overlay module to use.
-   */
-  module: string | false;
-  /**
-   * Path to a JS file that sets up the error overlay integration.
-   */
-  entry: string | false;
-  /**
-   * Path to a JS file that sets up the Webpack socket integration.
-   */
-  sockIntegration: import('type-fest').LiteralUnion<'wds' | 'whm' | 'wps' | false, string>;
-};
+export type NormalizedErrorOverlayOptions = import('type-fest').SetRequired<
+  ErrorOverlayOptions,
+  'entry' | 'module' | 'sockIntegration'
+>;
 export type ReactRefreshPluginOptions = {
   /**
    * Files to explicitly exclude from processing.
@@ -110,21 +70,8 @@ export type OverlayOverrides = {
    */
   overlay: false | NormalizedErrorOverlayOptions;
 };
-export type NormalizedPluginOptions = {
-  /**
-   * Enables the plugin forcefully.
-   */
-  forceEnable?: boolean | undefined;
-  /**
-   * Name of the library bundle.
-   */
-  library?: string | undefined;
-  /**
-   * Files to explicitly include for processing.
-   */
-  include: string | RegExp | Array<string | RegExp>;
-  /**
-   * Files to explicitly exclude from processing.
-   */
-  exclude: string | RegExp | Array<string | RegExp>;
-} & OverlayOverrides;
+export type NormalizedPluginOptions = import('type-fest').SetRequired<
+  import('type-fest').Except<ReactRefreshPluginOptions, 'overlay'>,
+  'exclude' | 'include'
+> &
+  OverlayOverrides;
