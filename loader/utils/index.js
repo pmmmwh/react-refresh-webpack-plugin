625d1b100a6682c47f048dcdc97cbf90243ed02a
Michael Mok <pmmmwh@gmail.com>
2021-03-29 02:07:15 +0200

    feat: implement simple template renderer, export utils

---
+++ loader/utils/index.js
@@ -0,0 +1,11 @@
+const getIdentitySourceMap = require('./getIdentitySourceMap');
+const getModuleSystem = require('./getModuleSystem');
+const normalizeOptions = require('./normalizeOptions');
+const renderTemplate = require('./renderTemplate');
+
+module.exports = {
+  getIdentitySourceMap,
+  getModuleSystem,
+  normalizeOptions,
+  renderTemplate,
+};
