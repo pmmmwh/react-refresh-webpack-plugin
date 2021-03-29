8a72f6a84d3ee4476c7a29685b20646a7ae456db
Michael Mok <pmmmwh@gmail.com>
2021-03-29 03:01:49 +0200

    test: fix jest-resolver to include js suffix

--- test/jest-resolver.js
+++ test/jest-resolver.js
@@ -4,11 +4,11 @@
  * @return {string}
  */
 function resolver(request, options) {
-  // This acts as a mock for `require.resolve('react-refresh/runtime')`,
+  // This acts as a mock for `require.resolve('react-refresh/runtime.js')`,
   // since the current mocking behaviour of Jest is not symmetrical,
   // i.e. only `require` is mocked but not `require.resolve`.
-  if (request === 'react-refresh/runtime') {
-    return 'react-refresh/runtime';
+  if (request === 'react-refresh/runtime.js') {
+    return 'react-refresh/runtime.js';
   }
 
   return options.defaultResolver(request, options);
