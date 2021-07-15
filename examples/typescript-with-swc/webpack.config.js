06dc619bfb23a710afffe4f2b1231b6701a7ab60
austaras <austaras@outlook.com>
2021-07-15 17:03:14 +0800

    docs: add swc-loader

---
+++ examples/typescript-with-swc/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  entry: {
+    main: './src/index.tsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        include: path.join(__dirname, 'src'),
+        use: [
+          {
+            loader: 'swc-loader',
+            options: {
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: true,
+                  dynamicImport: true,
+                },
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                    refresh: isDevelopment,
+                  },
+                },
+              },
+            },
+          },
+        ].filter(Boolean),
+      },
+    ],
+  },
+  plugins: [
+    isDevelopment && new ReactRefreshPlugin(),
+    new ForkTsCheckerWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './public/index.html',
+    }),
+  ].filter(Boolean),
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+};
