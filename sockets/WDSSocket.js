be2ad2413a3db77e3893af39fbe9ae922798aeed
Michael Mok <pmmmwh@gmail.com>
2021-08-19 00:32:43 +0200

    chore: fix formatting

--- sockets/WDSSocket.js
+++ sockets/WDSSocket.js
@@ -12,7 +12,7 @@ const getUrlFromParts = require('./utils/getUrlFromParts');
 function initWDSSocket(messageHandler, resourceQuery) {
   if (typeof __webpack_dev_server_client__ !== 'undefined') {
     let SocketClient = __webpack_dev_server_client__;
-    if (typeof __webpack_dev_server_client__.default !== "undefined") {
+    if (typeof __webpack_dev_server_client__.default !== 'undefined') {
       SocketClient = __webpack_dev_server_client__.default;
     }
 
