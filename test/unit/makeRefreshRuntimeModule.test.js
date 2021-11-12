9d9dd670d7d5b60545c03e64035f64c2ab644d61
Hikari Hayashi <rev.hikari@gmail.com>
2021-11-13 07:29:00 +0800

    fix: always use the regular function to avoid losing module context (#531)
    Co-authored-by: Michael Mok <pmmmwh@gmail.com>

--- test/unit/makeRefreshRuntimeModule.test.js
+++ test/unit/makeRefreshRuntimeModule.test.js
@@ -40,7 +40,7 @@ describe.skipIf(WEBPACK_VERSION !== 5, 'makeRefreshRuntimeModule', () => {
     expect(runtime).toMatchInlineSnapshot(`
 "__webpack_require__.i.push(function(options) {
 	var originalFactory = options.factory;
-	options.factory = function(moduleObject, moduleExports, webpackRequire) {
+	options.factory = function (moduleObject, moduleExports, webpackRequire) {
 		__webpack_require__.$Refresh$.setup(options.id);
 		try {
 			originalFactory.call(this, moduleObject, moduleExports, webpackRequire);
@@ -60,7 +60,7 @@ describe.skipIf(WEBPACK_VERSION !== 5, 'makeRefreshRuntimeModule', () => {
 				__webpack_require__.$Refresh$.cleanup(options.id)
 			}
 		}
-	}
+	};
 })
 
 __webpack_require__.$Refresh$ = {
@@ -119,7 +119,7 @@ __webpack_require__.$Refresh$ = {
     expect(runtime).toMatchInlineSnapshot(`
 "__webpack_require__.i.push((options) => {
 	const originalFactory = options.factory;
-	options.factory = (moduleObject, moduleExports, webpackRequire) => {
+	options.factory = function (moduleObject, moduleExports, webpackRequire) {
 		__webpack_require__.$Refresh$.setup(options.id);
 		try {
 			originalFactory.call(this, moduleObject, moduleExports, webpackRequire);
@@ -139,7 +139,7 @@ __webpack_require__.$Refresh$ = {
 				__webpack_require__.$Refresh$.cleanup(options.id)
 			}
 		}
-	}
+	};
 })
 
 __webpack_require__.$Refresh$ = {
