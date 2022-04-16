fb100f8131ee846ac9dbe31b0bc4ee7822fd9291
Michael Mok <pmmmwh@gmail.com>
2022-04-16 18:36:06 +0100

    Update index.tsx

--- examples/typescript-with-swc/src/index.tsx
+++ examples/typescript-with-swc/src/index.tsx
@@ -1,5 +1,5 @@
-import App from './App';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
