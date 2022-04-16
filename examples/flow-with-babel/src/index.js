b9adfd27fb87d2967ab4e100fe2288e122ae1071
Viktor Persson <vikpe@users.noreply.github.com>
2022-04-16 19:39:42 +0200

    Replace deprecated ReactDOM.render with Root.render in examples. (#626)

--- examples/flow-with-babel/src/index.js
+++ examples/flow-with-babel/src/index.js
@@ -1,6 +1,8 @@
 // @flow
 
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-render(<App />, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />);
