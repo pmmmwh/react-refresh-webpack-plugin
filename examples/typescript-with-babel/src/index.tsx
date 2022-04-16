304c83a34376f0d0650af1702cc71210b0858407
Viktor Persson <viktor.persson@arcsin.se>
2022-04-16 16:36:49 +0200

    Replace deprecated ReactDOM.render with Root.render in examples.

--- examples/typescript-with-babel/src/index.tsx
+++ examples/typescript-with-babel/src/index.tsx
@@ -1,4 +1,6 @@
-import { render } from 'react-dom';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
-render(<App />, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(<App />);
