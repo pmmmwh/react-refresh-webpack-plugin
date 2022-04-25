6e1f515d18e643460a82dffa402fc424fe2f47df
Michael Mok <pmmmwh@gmail.com>
2022-04-26 00:07:40 +0200

    chore: format

--- sockets/utils/getCurrentScriptSource.js
+++ sockets/utils/getCurrentScriptSource.js
@@ -5,7 +5,7 @@
 function getCurrentScriptSource() {
   // `document.currentScript` is the most accurate way to get the current running script,
   // but is not supported in all browsers (most notably, IE).
-  if ("currentScript" in document) {
+  if ('currentScript' in document) {
     // In some cases, `document.currentScript` would be `null` even if the browser supports it:
     // e.g. asynchronous chunks on Firefox.
     // We should not fallback to the list-approach as it would not be safe.
@@ -15,9 +15,12 @@ function getCurrentScriptSource() {
   // Fallback to getting all scripts running in the document,
   // and finding the last one injected.
   else {
-    const scriptElementsWithSrc = Array.prototype.filter.call(document.scripts || [], function (elem) {
-      return elem.getAttribute('src');
-    });
+    const scriptElementsWithSrc = Array.prototype.filter.call(
+      document.scripts || [],
+      function (elem) {
+        return elem.getAttribute('src');
+      }
+    );
     if (!scriptElementsWithSrc.length) return;
     const currentScript = scriptElementsWithSrc[scriptElementsWithSrc.length - 1];
     return currentScript.getAttribute('src');
