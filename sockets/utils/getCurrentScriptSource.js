d859c9f1f6b54d3dae7cadd30eebf820083cad17
Michael Mok <pmmmwh@gmail.com>
2022-04-26 00:25:01 +0200

    fix: make current script detection more robust on edge cases (#630)

--- sockets/utils/getCurrentScriptSource.js
+++ sockets/utils/getCurrentScriptSource.js
@@ -5,16 +5,23 @@
 function getCurrentScriptSource() {
   // `document.currentScript` is the most accurate way to get the current running script,
   // but is not supported in all browsers (most notably, IE).
-  if (document.currentScript) {
+  if ('currentScript' in document) {
+    // In some cases, `document.currentScript` would be `null` even if the browser supports it:
+    // e.g. asynchronous chunks on Firefox.
+    // We should not fallback to the list-approach as it would not be safe.
+    if (document.currentScript == null) return;
     return document.currentScript.getAttribute('src');
   }
-
-  // Fallback to getting all scripts running in the document.
-  const scriptElements = document.scripts || [];
-  const scriptElementsWithSrc = Array.prototype.filter.call(scriptElements, function (elem) {
-    return elem.getAttribute('src');
-  });
-  if (scriptElementsWithSrc.length) {
+  // Fallback to getting all scripts running in the document,
+  // and finding the last one injected.
+  else {
+    const scriptElementsWithSrc = Array.prototype.filter.call(
+      document.scripts || [],
+      function (elem) {
+        return elem.getAttribute('src');
+      }
+    );
+    if (!scriptElementsWithSrc.length) return;
     const currentScript = scriptElementsWithSrc[scriptElementsWithSrc.length - 1];
     return currentScript.getAttribute('src');
   }
