5e03de2da574e4028d94ab1978e6d400a69d2367
Eleni Arvanitis Konior <eleni.arvanitis@me.com>
2024-02-17 12:09:55 -0500

    add helper method to return WDS version based on node version

--- test/jest-environment.js
+++ test/jest-environment.js
@@ -9,7 +9,7 @@ class SandboxEnvironment extends NodeEnvironment {
 
     this.global.__DEBUG__ = yn(process.env.DEBUG);
     this.global.WEBPACK_VERSION = parseInt(process.env.WEBPACK_VERSION || '5', 10);
-    this.global.WDS_VERSION = semver.major(process.version) < 12 ? 3 : 4 || 5;
+    this.global.WDS_VERSION = this.resolveWdsVersion(semver.major(process.version));
 
     const wsEndpoint = process.env.PUPPETEER_WS_ENDPOINT;
     if (!wsEndpoint) {
@@ -19,6 +19,16 @@ class SandboxEnvironment extends NodeEnvironment {
     this.global.browser = await puppeteer.connect({
       browserWSEndpoint: wsEndpoint,
     });
+  }
+
+  resolveWdsVersion(nodeVersion) {
+    if (nodeVersion < 12) {
+      return 3;
+    } else if (nodeVersion < 18) {
+      return 4;
+    }
+
+    return 5;
   }
 
   async teardown() {
