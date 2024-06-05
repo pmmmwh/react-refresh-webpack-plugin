138c70e78c4df47d231515d66c0ab07c0f9879f3
Michael Mok <pmmmwh@gmail.com>
2024-06-05 18:03:05 +0200

    refactor: migrate from ansi-html to anser (#854)

--- overlay/components/PageHeader.js
+++ overlay/components/PageHeader.js
@@ -17,9 +17,9 @@ const theme = require('../theme.js');
  */
 function PageHeader(document, root, props) {
   const pageHeaderContainer = document.createElement('div');
-  pageHeaderContainer.style.background = '#' + theme.dimgrey;
+  pageHeaderContainer.style.background = theme['dark-background'];
   pageHeaderContainer.style.boxShadow = '0 1px 4px rgba(0, 0, 0, 0.3)';
-  pageHeaderContainer.style.color = '#' + theme.white;
+  pageHeaderContainer.style.color = theme.white;
   pageHeaderContainer.style.left = '0';
   pageHeaderContainer.style.right = '0';
   pageHeaderContainer.style.padding = '1rem 1.5rem';
@@ -30,7 +30,7 @@ function PageHeader(document, root, props) {
 
   const title = document.createElement('h3');
   title.innerText = props.title;
-  title.style.color = '#' + theme.red;
+  title.style.color = theme.red;
   title.style.fontSize = '1.125rem';
   title.style.lineHeight = '1.3';
   title.style.margin = '0';
@@ -41,7 +41,7 @@ function PageHeader(document, root, props) {
 
     const message = document.createElement('span');
     message.innerText = props.message;
-    message.style.color = '#' + theme.white;
+    message.style.color = theme.white;
     message.style.wordBreak = 'break-word';
     pageHeaderContainer.appendChild(message);
   }
