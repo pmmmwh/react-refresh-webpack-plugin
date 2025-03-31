3e21d36fca8f281cd9cb8a92e9cd33326b4857a3
Michael Mok <pmmmwh@gmail.com>
2025-03-31 02:08:24 +0200

    ci: migrate to github actions (#901)
    * ci: migrate to github actions
    
    * test: support skipping browser setup

--- test/jest-environment.js
+++ test/jest-environment.js
@@ -1,9 +1,8 @@
-const { TestEnvironment } = require('jest-environment-node');
-const puppeteer = require('puppeteer');
+const { TestEnvironment: NodeEnvironment } = require('jest-environment-node');
 const semver = require('semver');
 const yn = require('yn');
 
-class SandboxEnvironment extends TestEnvironment {
+class TestEnvironment extends NodeEnvironment {
   async setup() {
     await super.setup();
 
@@ -15,24 +14,7 @@ class SandboxEnvironment extends TestEnvironment {
         : semver.major(process.version) < 18 || this.global.WEBPACK_VERSION === 4
           ? 4
           : 5;
-
-    const wsEndpoint = process.env.PUPPETEER_WS_ENDPOINT;
-    if (!wsEndpoint) {
-      throw new Error('Puppeteer wsEndpoint not found!');
-    }
-
-    this.global.browser = await puppeteer.connect({
-      browserWSEndpoint: wsEndpoint,
-    });
-  }
-
-  async teardown() {
-    await super.teardown();
-
-    if (this.global.browser) {
-      await this.global.browser.disconnect();
-    }
   }
 }
 
-module.exports = SandboxEnvironment;
+module.exports = TestEnvironment;
--- test/jest-global-setup.js
+++ test/jest-global-setup.js
@@ -2,14 +2,19 @@ const puppeteer = require('puppeteer');
 const yn = require('yn');
 
 async function setup() {
-  const browser = await puppeteer.launch({
-    devtools: yn(process.env.DEBUG, { default: false }),
-    headless: yn(process.env.HEADLESS, { default: false }),
-  });
+  if (yn(process.env.BROWSER, { default: true })) {
+    const browser = await puppeteer.launch({
+      devtools: yn(process.env.DEBUG, { default: false }),
+      headless: yn(process.env.HEADLESS, {
+        // Force headless mode in CI environments
+        default: yn(process.env.CI, { default: false }),
+      }),
+    });
 
-  global.__BROWSER_INSTANCE__ = browser;
+    global.__BROWSER_INSTANCE__ = browser;
 
-  process.env.PUPPETEER_WS_ENDPOINT = browser.wsEndpoint();
+    process.env.PUPPETEER_WS_ENDPOINT = browser.wsEndpoint();
+  }
 }
 
 module.exports = setup;
